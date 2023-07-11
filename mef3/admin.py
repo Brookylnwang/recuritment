fe6ea8d188e89d26ae506a497744ec9c0b168be0
Brooklnwang <1647946421@qq.com>
2023-07-11 23:50:52 +0800

    used django version >= 4. Changed some configuration 添加了一个导入功能，暂时还没完全实现

--- mef3/admin.py
+++ mef3/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from mef3.models import Mef
+import csv
+import logging
+
+
+def upload_csv_as_model(modeladmin, ):
+    f = open("/Users/wangsheng/Downloads/wang.txt", encoding="utf-8")
+    reader = csv.reader(f)
+    mef3_content_list = []
+    for row in reader:
+        # logging.error("This row content is %s" % str(row))
+        mef3_content_list.append(str(row))
+    logging.info("This row content is %s" % str(mef3_content_list[0]))
+
+
+
+upload_csv_as_model.short_description = u"导入mef3信息"
 
 # Register your models here.
 class MefAdmin(admin.ModelAdmin):
     list_display = ("mef_server_name", "mef_date", "mef_report_file")
+
+    actions = [upload_csv_as_model]
 
 admin.site.register(Mef, MefAdmin)
