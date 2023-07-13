70047ef232a432a923f8c216f4471958bb85297a
Brooklnwang <1647946421@qq.com>
2023-07-13 23:41:11 +0800

    used django version >= 4. Changed some configuration 在收集mef3文件模块添加了一个更改状态的功能 添加了一个model 字段：文件路径状态 还有点小bug

--- mef3/admin.py
+++ mef3/admin.py
@@ -3,9 +3,18 @@ from mef3.models import Mef
 import csv
 import logging
 
+@admin.action(description="更改状态")
+def make_path_status(modeladmin, request, queryset):
+    print(queryset.values("mef_report_file_status"))
+    for i in queryset.values("mef_report_file_status"):
+        if str(i["mef_report_file_status"]) == "1":
+            queryset.update(mef_report_file_status="2")
+        else:
+            queryset.update(mef_report_file_status="1")
+
 
 def upload_csv_as_model(modeladmin, ):
-    f = open("/Users/wangsheng/Downloads/wang.txt", encoding="utf-8")
+    f = open("/Users/wangsheng/Downloads/mef3.csv", encoding="utf-8")
     reader = csv.reader(f)
     mef3_content_list = []
     for row in reader:
@@ -19,8 +28,8 @@ upload_csv_as_model.short_description = u"导入mef3信息"
 
 # Register your models here.
 class MefAdmin(admin.ModelAdmin):
-    list_display = ("mef_server_name", "mef_date", "mef_report_file")
+    list_display = ("mef_server_name", "mef_date", "mef_report_file", "mef_report_file_status")
 
-    actions = [upload_csv_as_model]
+    actions = [upload_csv_as_model, make_path_status]
 
 admin.site.register(Mef, MefAdmin)
--- mef3/models.py
+++ mef3/models.py
@@ -4,13 +4,9 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-ServerGroup = [
-    (0, "WAVE1组"),
-    (1, "WAVE2组"),
-    (2, "DEV组"),
-    (3, "windows组"),
-    (4, "AIOps组"),
-    (5, "其他组")
+STATUS_CHOICES = [
+    ("1", "存在"),
+    ("2", "不存在"),
 ]
 
 
@@ -18,6 +14,7 @@ class Mef(models.Model):
     mef_date = models.DateTimeField(verbose_name="获取mef时间", null=True)
     mef_server_name = models.CharField(max_length=20, blank=False, verbose_name="服务器名称")
     mef_report_file = models.FileField(upload_to='uploads/%Y/%m/%d/',blank=True, max_length=254, verbose_name="文件名路径")
+    mef_report_file_status = models.CharField(max_length=10, choices=STATUS_CHOICES, blank=True, verbose_name="文件路径的状态")
 
     class Meta:
         verbose_name = _('收集mef3')
