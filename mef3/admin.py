aad470d688f10748cda014f0173c9ce863d9231d
Brooklnwang <1647946421@qq.com>
2023-07-21 22:32:24 +0800

    used django version >= 4. Changed some configuration 更改文件路径状态的bug问题解决了，还在研究上传csv文件的功能

--- mef3/admin.py
+++ mef3/admin.py
@@ -1,35 +1,71 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.urls import path
+from django import forms
+
 from mef3.models import Mef
 import csv
 import logging
 
+
+# exportable_fields = ("mef_report_file_status")
+
+
 @admin.action(description="更改状态")
 def make_path_status(modeladmin, request, queryset):
-    print(queryset.values("mef_report_file_status"))
-    for i in queryset.values("mef_report_file_status"):
-        if str(i["mef_report_file_status"]) == "1":
-            queryset.update(mef_report_file_status="2")
-        else:
-            queryset.update(mef_report_file_status="1")
+    server_names = ""
+    for obj in queryset:
+        if str(obj.mef_report_file_status) == "1":
+           obj.mef_report_file_status = 2
+        elif str(obj.mef_report_file_status) == "2":
+           obj.mef_report_file_status = 1
+        obj.save()
+        server_names = server_names + "," + obj.mef_server_name
+    if queryset.count() > 3:
+        messages.success(request, "一大批服务器状态已改变")
+    else:
+        messages.success(request, "服务器%s状态已改变" % server_names)
+
+# @admin.action(description="导入mef3信息")
+#def upload_csv_as_model(modeladmin, request, queryset):
+#    pass
 
 
-def upload_csv_as_model(modeladmin, ):
-    f = open("/Users/wangsheng/Downloads/mef3.csv", encoding="utf-8")
-    reader = csv.reader(f)
-    mef3_content_list = []
-    for row in reader:
-        # logging.error("This row content is %s" % str(row))
-        mef3_content_list.append(str(row))
-    logging.info("This row content is %s" % str(mef3_content_list[0]))
+def get_urls(self):
+    urls = super().get_urls()
+    my_urls = [
+        path('import-csv/', self.import_csv)
+    ]
+    return my_urls + urls
 
 
+@admin.action(description="导入mef3")
+def import_csv(self, request):
+    if request.method == "POST":
+        print(1)
+        csv_file = request.FILES["csv_file"]
+        reader = csv.reader(csv_file)
+        print(reader)
 
-upload_csv_as_model.short_description = u"导入mef3信息"
+        messages.success(request, "Your csv file upload successfully")
+        return redirect("..")
+    form = CsvImportForm()
+    payload = {"form": form}
+    return render(
+        request, "templates/mef3/csv_form.html", payload
+    )
+    # logging.info("This row content is %s" % str(mef3_content_list[0]))
+
+
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+
 
 # Register your models here.
 class MefAdmin(admin.ModelAdmin):
     list_display = ("mef_server_name", "mef_date", "mef_report_file", "mef_report_file_status")
 
-    actions = [upload_csv_as_model, make_path_status]
+    actions = [import_csv, make_path_status]
 
 admin.site.register(Mef, MefAdmin)
